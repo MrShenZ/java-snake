56894c6d04d3a967685b7a3dfe95a88abf5ed25d
Mr.Shen <995855732@qq.com>
2018-10-21 20:26:26 +0800

    snake second lesson enum

--- src/main/java/com/tianmaying/snake/Grid.java
+++ src/main/java/com/tianmaying/snake/Grid.java
@@ -2,23 +2,5 @@ package com.tianmaying.snake;
 
 
 public class Grid {
-    private Snake snake;
-    {
-    	snake=new Snake();
-    }
-    private int width;
-    private int height;
-    public Grid(int width, int height) {
-        this.width=width;
-        this.height=height;
-    }
-    public Snake getSnake(){
-    	return snake;
-    }
-    public int getWidth(){
-    	return width;
-    }
-    public int getHeight(){
-    	return height;
-    }
+
 }
