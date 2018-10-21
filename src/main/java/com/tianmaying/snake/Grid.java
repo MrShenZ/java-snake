84036f637460f3cda4c2e70d3072a0dbb1dc0607
Mr.Shen <995855732@qq.com>
2018-10-21 12:04:31 +0800

    snake first lesson Grid

--- src/main/java/com/tianmaying/snake/Grid.java
+++ src/main/java/com/tianmaying/snake/Grid.java
@@ -2,9 +2,20 @@ package com.tianmaying.snake;
 
 
 public class Grid {
-    
+    private Snake snake=null;
+    private int width;
+    private int height;
     public Grid(int width, int height) {
-        // your code here
+        this.width=width;
+        this.height=height;
     }
-
+    public Snake getSnake(){
+    	return snake;
+    }
+    public int getWidth(){
+    	return width;
+    }
+    public int getHeight(){
+    	return height;
+    }
 }
